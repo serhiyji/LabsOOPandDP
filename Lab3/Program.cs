5eefbbe54a937f43a210ff615ba09a5b3fb211ef
serhiyji <serhiy22823@gmail.com>
2023-09-20 11:21:55 +0300

    Lab3 update

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,23 +1,25 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 
 namespace Lab3
 {
     public class Formula
     {
+        public static long Max = 0;
         private float A;
         private float B;
         private float X;
+        private decimal[] list;
         public Formula(float a = 1, float b = 1, float x = 1)
         {
             this.A = a;
             this.B = b;
             this.X = x;
-            Console.WriteLine($"Ctor : {A}, {B}, {X}");
+            list = new decimal[1000];
+
         }
         ~Formula()
         {
-            Console.WriteLine($"Dtor : {A}, {B}, {X}");
+            Max = Math.Max(Max, GC.GetTotalMemory(true));
         }
         public double Calculate()
         {
@@ -26,34 +28,26 @@ namespace Lab3
     }
     internal class Program
     {
-        private static void Task1()
-        {
-            Console.Write("Count >> ");
-            int count = int.Parse(Console.ReadLine());
-            Formula[] formulas = new Formula[count];
-            for (int i = 0; i < count; i++)
-            {
-                Console.WriteLine($"Enter parametrs {i + 1}");
-
-                Console.Write("a >> ");
-                float a = float.Parse(Console.ReadLine());
-                Console.Write("b >> ");
-                float b = float.Parse(Console.ReadLine());
-                Console.Write("x >> ");
-                float x = float.Parse(Console.ReadLine());
-
-                formulas[i] = new Formula(a, b, x);
-            }
-            Console.WriteLine("--------");
-            for (int i = 0; i < formulas.Length; i++)
-            {
-                Console.WriteLine($"{i + 1} : {formulas[i].Calculate()}");
-            }
-        }
         static void Main(string[] args)
         {
-            Task1();
-            GC.Collect();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            long d1 = GC.GetTotalMemory(true);
+            Console.WriteLine($"Перед виконанням програми {d1}");
+            Console.Write("Count >> ");
+            int count = int.Parse(Console.ReadLine());
+            Random random = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                float a = i;
+                float b = random.Next(0, int.MaxValue);
+                float x = random.Next(0, int.MaxValue);
+                Formula formula = new Formula(a, b, x);
+            }
+            long d2 = GC.GetTotalMemory(true);
+            Console.WriteLine($"Після виконанням програми {d2}");
+            Console.WriteLine($"Максимальна кількість памятізайнята програмою {Formula.Max}");
             Console.ReadLine();
         }
     }
