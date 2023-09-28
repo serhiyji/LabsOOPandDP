0b1b9f4613fb0f5b594728ddb07d5a44f5cd4ba6
serhiyji <serhiy22823@gmail.com>
2023-09-28 10:20:12 +0300

    Add Lab8

---
+++ Lab8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lab8
+{
+    public static class ExtentionString
+    {
+        public static int CountOccurrences(this string str, string pattern)
+        {
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(str);
+            return matches.Count;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "This is an example, and this is another example.";
+            string pattern = "example";
+            int count = input.CountOccurrences(pattern);
+            Console.WriteLine($"Number of occurrences of '{pattern}': {count}");
+        }
+    }
+}
